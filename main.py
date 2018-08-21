694d524da31703c77f8ebad3d85cac82afbbbf40
XieBenyi <xieby1@outlook.com>
2018-08-21 18:52:05 +0800

    New feature: + Priority list + Auto re-connect New dependent lib: * pybluez

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import NetworkManager
 import time
+import bluetooth
 
 
 def isConnected(state):
@@ -26,55 +27,65 @@ def cmpbdhwAddr(bdAddr, hwAddr):
         if hwAddrDec != bdAddrDec:
             return False
     return True
+################### END OF FUNC DEF #########################
+
+# Add your host device hardware address here
+# TIP: the entry nearer the list top has higher priority
+priorityList = {
+                "30:3A:64:E7:76:00",    # laptop
+                "2C:57:31:07:C5:61"     # Tse Hotspot
+                }
 
 
-# hwAddr = "2C:57:31:07:C5:61"  # Tse Hotspot
-hwAddr = "30:3A:64:E7:76:00"  # laptop
-
-# firstly, find the connection by hwAddr.
-connections = NetworkManager.Settings.ListConnections()
-for x in connections:
-    if 'bluetooth' in x.GetSettings():
-        if cmpbdhwAddr(x.GetSettings()['bluetooth']['bdaddr'], hwAddr):
-            connection = x
-            break
-
-# secondly, find the device according to hwAddr.
-devices = NetworkManager.NetworkManager.GetDevices()
-for x in devices:
-    if type(x) == NetworkManager.Bluetooth:
-        if x.HwAddress == hwAddr:
-            device = x
-            break
-
-
-
-temp_ds = device.State
-prevState = isConnected(temp_ds)
+prevState = False
 #logFile = open("state logs", 'a')
 #logFile.write(str(temp_ds) + " " + NetworkManager.const('device_state', temp_ds) + '\n')
 #logFile.close()
-if not prevState:
-    NetworkManager.NetworkManager.ActivateConnection(connection, device, "/")
-
+hwAddr = ""
 maxSleppTime = 64  # in second
 sleepTime = 1  # in second
 while True:
-    temp_ds = device.State
-    curState = isConnected(temp_ds)
+    if len(hwAddr) == 0:
+        curState = False
+    else:
+        curState = isConnected(device.State)
     if prevState and curState:
-        time.sleep(sleepTime)
         sleepTime = min(maxSleppTime, sleepTime * 2)
-        continue
     elif not prevState and not curState:
-        NetworkManager.NetworkManager.ActivateConnection(connection, device, "/")
-        time.sleep(sleepTime)
+        available_services = bluetooth.find_service(uuid=bluetooth.NAP_CLASS)
+        hwAddr = ""
+        for list_device in priorityList:
+            for available_service in available_services:
+                if available_service["host"] == list_device:
+                    hwAddr = list_device
+                    break
+            if len(hwAddr) > 0:
+                break
+        if len(hwAddr)>0:
+            # firstly, find the connection by hwAddr.
+            connections = NetworkManager.Settings.ListConnections()
+            for x in connections:
+                if 'bluetooth' in x.GetSettings():
+                    if cmpbdhwAddr(x.GetSettings()['bluetooth']['bdaddr'], hwAddr):
+                        connection = x
+                        break
+
+            # secondly, find the device according to hwAddr.
+            devices = NetworkManager.NetworkManager.GetDevices()
+            for x in devices:
+                if type(x) == NetworkManager.Bluetooth:
+                    if x.HwAddress == hwAddr:
+                        device = x
+                        break
+
+            NetworkManager.NetworkManager.ActivateConnection(connection, device, "/")
+
         sleepTime = min(maxSleppTime, sleepTime * 2)
-        prevState = curState
-        continue
-    elif prevState and not curState:
+    else:  # not prevState and curState or prevState and not curState::
         sleepTime = 1
-        prevState = curState
+
+    time.sleep(sleepTime)
+    prevState = curState
  #   logFile = open("state logs", 'a')
   #  logFile.write(str(temp_ds) + " " + NetworkManager.const('device_state', temp_ds) + '\n')
    # logFile.close()
